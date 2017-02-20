5719093d163e3881a0ca67bea9dd9f373987cf79
rancheng111 <fff.123@foxmail.com>
2017-02-20 16:41:20 +0800

    v2

--- frontEnd/src/js/index.js
+++ frontEnd/src/js/index.js
@@ -1,10 +1,41 @@
-
-debugger;
-alert(33)
+//react
 import React from 'react'
+import { render } from 'react-dom'
+import { Router, Route, Link ,hashHistory} from 'react-router';
 
-const a = 1;
 
-let bbb =1;
+//compents
+                    //注意点   后面不能写.js后缀
+import Index from './module/login/index';
+import Login from './module/unlogin/login';
+import ProductList from   './module/login/product/product_list';
+import ProductAdd  from   './module/login/product/product_add';
+import ProductDetail from './module/login/product/product_detail';
+import orderList from './module/login/order/order_list';
 
-//require('react')
+
+
+
+
+
+
+render((
+        <div>
+            <Router history={hashHistory}>
+                <Route path="/" component={Login}></Route>
+                <Route path="/login" component={Login}></Route>
+
+                <Route path="/index" component={Index}>
+                    <Route path="/productList" component={ProductList} />
+                    <Route path="/productAdd"  component={ProductAdd} />
+                    <Route path="/productDetail" component={ProductDetail} />
+
+                    <Route path="/orderList" component={orderList} />
+                </Route>
+            </Router>    
+        </div>
+    
+), document.getElementById('app'));
+
+
+
